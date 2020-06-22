e8bd383f8eb07081cba4f29b2537318b6952e0ef
hiinnn <hin_pansa@hotmail.com>
2020-06-22 13:43:41 +0700

    updating style

--- src/components/list-group/board-list.js
+++ src/components/list-group/board-list.js
@@ -1,38 +1,38 @@
-import React, { useContext, useState, useRef, useEffect } from 'react'
-import styled from 'styled-components'
-// import Draggable from 'react-draggable'
+import React from 'react'
+
 import { Droppable, Draggable } from 'react-beautiful-dnd'
 import { BoardContext } from '../../context/board-context/board-context'
 import Card from './board-card'
 import { TextareaAutosize, Button, Popover, Divider, } from '@material-ui/core'
+import * as ListStyle from './list-styled'
 
 
 export default function List(props) {
-    const { boardState, boardDispatch } = useContext(BoardContext)
+    const { boardState, boardDispatch } = React.useContext(BoardContext)
     const list = boardState.lists[props.index]
 
     // Edit listname & New Card
-    const listNameRef = useRef(null)
-    const [edittingListName, setEdittingListName] = useState(false)
-    const [listName, renameList] = useState({
+    const listNameRef = React.useRef(null)
+    const [edittingListName, setEdittingListName] = React.useState(false)
+    const [listName, renameList] = React.useState({
         oldListName: list.name,
         newListName: list.name,
     })
 
-    const newCardRef = useRef(null)
-    const [newCardName, setNewCardName] = useState('')
-    const [edittingCardName, setEdittingCardName] = useState(false)
+    const newCardRef = React.useRef(null)
+    const [newCardName, setNewCardName] = React.useState('')
+    const [edittingCardName, setEdittingCardName] = React.useState(false)
 
-    const [selectFocus, setSelectFocus] = useState(null)
+    const [selectFocus, setSelectFocus] = React.useState(null)
 
     // List popover & New card popover 
-    const [anchorEl, setAnchorEl] = useState({
+    const [anchorEl, setAnchorEl] = React.useState({
         listOpen: false,
         newCardOpen: false,
         element: null
     })
 
-    useEffect(() => {
+    React.useEffect(() => {
         if (selectFocus === 'list') {
             listNameRef.current.focus()
         }
@@ -112,18 +112,17 @@ export default function List(props) {
         // do something
     }
 
-
-    console.log('re-render');
+    console.log('re-render', ListStyle);
 
     return (
         <Draggable draggableId={props.listId} index={props.index}>
             {(provided) => (
-                <ListWrapper
+                <ListStyle.ListWrapper
                     ref={provided.innerRef}
                     {...provided.draggableProps}
                 >
-                    <ListContent>
-                        <HeaderField {...provided.dragHandleProps}>
+                    <ListStyle.ListContent>
+                        <ListStyle.HeaderField {...provided.dragHandleProps}>
                             <div
                                 className={`drag-target ${edittingListName ? 'hide' : ''}`}
                                 onClick={() => {
@@ -156,7 +155,7 @@ export default function List(props) {
                                         horizontal: 'left',
                                     }}
                                 >
-                                    <PopOverList>
+                                    <ListStyle.PopOverList>
                                         <div className='pop-over-header'>
                                             <div className='list-act'>List Actions</div>
                                             <div className='x-button' onClick={() => closeAnchor('LIST')}>X</div>
@@ -192,14 +191,14 @@ export default function List(props) {
                                             </ul>
 
                                         </div>
-                                    </PopOverList>
+                                    </ListStyle.PopOverList>
                                 </Popover>
                             </div>
-                        </HeaderField>
+                        </ListStyle.HeaderField>
 
                         <Droppable droppableId={props.listId} type='card'>
                             {(provided, snapshot) => (
-                                <CardList
+                                <ListStyle.CardList
                                     ref={provided.innerRef}
                                     {...provided.droppableProps}
                                     isDraggingOver={snapshot.isDraggingOver}
@@ -217,10 +216,10 @@ export default function List(props) {
                                         })
                                     }
                                     {provided.placeholder}
-                                </CardList>
+                                </ListStyle.CardList>
                             )}
                         </Droppable>
-                        <AddCard>
+                        <ListStyle.AddCard>
                             <div
                                 className={`add-composer ${!edittingCardName ? '' : 'hide'}`}
                                 onClick={() => {
@@ -272,7 +271,7 @@ export default function List(props) {
                                                 horizontal: 'left',
                                             }}
                                         >
-                                            <PopOverList>
+                                            <ListStyle.PopOverList>
                                                 <div className='pop-over-header'>
                                                     <div className='list-act'>Options</div>
                                                     <div className='x-button' onClick={() => closeAnchor('LIST')}>X</div>
@@ -287,224 +286,15 @@ export default function List(props) {
                                                         <li>Position</li>
                                                     </ul>
                                                 </div>
-                                            </PopOverList>
+                                            </ListStyle.PopOverList>
                                         </Popover>
                                     </div>
                                 </div>
                             </div>
-                        </AddCard>
-                    </ListContent>
-                </ListWrapper>
+                        </ListStyle.AddCard>
+                    </ListStyle.ListContent>
+                </ListStyle.ListWrapper>
             )}
         </Draggable>
     )
 }
-const ListWrapper = styled.div`
-    max-height: 95%;
-    min-width: 250px;
-    max-width: 280px;
-    margin-right: 10px;
-    border-radius: 10px;
-    padding-bottom: 10px;
-    background-color: transparent;
-`
-
-const ListContent = styled.div`
-    display: flex;
-    max-height: 100%;
-    position: relative;
-    border-radius: 5px;
-    flex-direction: column;
-    background-color: #ebecf0;
-`
-
-const HeaderField = styled.div`
-    min-height: 40px;
-    padding: 10px 8px;
-    font-size: 20px;
-    position: relative;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-
-    .MuiButton-root {
-        min-width: 30px;
-        width: 40px;
-        height: 40px;
-    }
-
-    textarea {
-        width: 180px;
-        resize: none;
-        font-size: 18px;
-        font-weight: 600;
-        word-break: keep-all;
-        border: 0;
-        padding: 8px 8px;
-        border-radius: 5px;
-        background-color: transparent;
-
-        :focus {
-            outline-color: #2F3F8F;
-            background-color: white;
-        }
-    }
-    .drag-target {
-        cursor: pointer;
-        position: absolute;
-        top: 0;
-        left: 0;
-        right: 0;
-        bottom: 0;
-    }
-    .hide {
-        display: none;
-    }
-`
-const CardList = styled.div`
-    padding: 10px;
-    overflow-y: auto;
-    background-color: ${props => props.isDraggingOver ? 'green' : 'transparent'}
-`
-
-const AddCard = styled.div`
-    min-height: 40px;
-    margin: 10px 8px;
-    display: table;
-
-    .add-composer {
-        width: 100%;
-        height: 40px;
-        padding-left: 10px;
-        border-radius: 3px;
-        font-size: 15px;
-        display: table-cell;
-        vertical-align: middle;
-        color: #5e6c84;
-        cursor: pointer;
-
-        :hover {
-            background-color: rgba(9,30,66,.08);
-        }
-    }
-
-    .new-card-wrapper {
-        width: 100%;
-    }
-
-    .new-card-wrapper textarea {
-        min-height: 20px;
-        width: calc(100% - 20px);
-        
-        border: 0;
-        resize: none;
-        padding: 10px;
-        font-size: 10px;
-        background-color: white;
-    }
-
-    .MuiButton-root {
-        min-width: 30px;
-        text-transform: none;
-    }
-
-    .add-card-wrapper {
-        height: 40px;
-    }
-
-    .add-button {
-        min-width: 60px;
-        min-height: 40px;
-        overflow: hidden;
-        display: inline-block;
-
-        color: white;
-        background-color: #61AA4F;
-
-        :hover {
-            background-color: #65BF4A;
-        }
-    }
-    
-    .x-button {
-        min-width: 40px;
-        min-height: 40px;
-
-        color: #5e6c84;
-        font-size: 15px;
-        line-height: 40px;
-        text-align: center;
-
-        display: inline-block;
-
-        :hover {
-            color: #405070;
-            cursor: pointer;
-        }
-    }
-
-    .ellipsis-button {
-        width: 50px;
-        min-height: 30px;
-        margin-top: 10px;
-
-        float: right;
-        color: #5e6c84;
-        font-size: 15px;
-        text-align: center;
-    }
-
-    .hide {
-        display: none;
-    }
-`
-
-const PopOverList = styled.div`
-    width: 280px;
-    font-size: 14px;
-
-    padding-bottom: 10px;
-
-    .pop-over-header {
-        text-align: center;
-        position: relative;
-
-        color: #5e6c84;
-    }
-
-    .pop-over-header .list-act {
-        line-height: 40px;
-    }
-
-    .pop-over-header .x-button {
-        width: 40px;
-        height: 40px;
-        line-height: 40px;
-
-        top: 0;
-        right: 0;
-        position: absolute;
-
-        cursor: pointer;
-
-        :hover {
-            color: #1e2c44;
-        }
-    }
-
-    ul {
-        list-style-type: none;
-        margin: 0;
-        padding: 0;
-    }
-    
-    li {
-        cursor: pointer;
-        line-height: 30px;
-        padding-left: 10%;
-
-        :hover {
-            background-color: rgba(9,30,66,.08);
-        }
-    }
-`
