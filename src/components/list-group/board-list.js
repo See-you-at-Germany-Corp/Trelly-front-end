58846a4af1622f1b194a7f3e5d306bf9f16e2ed1
hiinnn <hin_pansa@hotmail.com>
2020-06-22 12:05:05 +0700

    pop-over structure

--- src/components/list-group/board-list.js
+++ src/components/list-group/board-list.js
@@ -1,60 +1,71 @@
 import React, { useContext, useState, useRef, useEffect } from 'react'
 import styled from 'styled-components'
-
 // import Draggable from 'react-draggable'
 import { Droppable, Draggable } from 'react-beautiful-dnd'
 import { BoardContext } from '../../context/board-context/board-context'
 import Card from './board-card'
-import { TextareaAutosize, Button, } from '@material-ui/core'
+import { TextareaAutosize, Button, Popover, Divider, } from '@material-ui/core'
+
 
 export default function List(props) {
     const { boardState, boardDispatch } = useContext(BoardContext)
-
     const list = boardState.lists[props.index]
 
-    const nameRef = useRef(null)
-    const [name, rename] = useState({
-        oldName: list.name,
-        newName: list.name
+    // Edit listname & New Card
+    const listNameRef = useRef(null)
+    const [edittingListName, setEdittingListName] = useState(false)
+    const [listName, renameList] = useState({
+        oldListName: list.name,
+        newListName: list.name,
     })
-    const [edittingName, setEdittingName] = useState(false)
 
     const newCardRef = useRef(null)
     const [newCardName, setNewCardName] = useState('')
     const [edittingCardName, setEdittingCardName] = useState(false)
 
+    const [selectFocus, setSelectFocus] = useState(null)
+
+    // List popover & New card popover 
+    const [anchorEl, setAnchorEl] = useState({
+        listOpen: false,
+        newCardOpen: false,
+        element: null
+    })
 
     useEffect(() => {
-        if (edittingName) {
-            nameRef.current.focus()
+        if (selectFocus === 'list') {
+            listNameRef.current.focus()
+        }
+        else if (selectFocus === 'card') {
+            newCardRef.current.focus()
         }
     });
 
     const handleChangeName = e => {
-        rename({
-            oldName: name.oldName,
-            newName: e.target.value
+        renameList({
+            oldListName: listName.oldListName,
+            newListName: e.target.value,
         })
     }
-
     const handleNewCardName = e => {
         setNewCardName(e.target.value)
     }
 
     const listNameBlur = e => {
-        setEdittingName(!edittingName)
+        setEdittingListName(!edittingListName)
+        setSelectFocus('')
 
-        if (!name.newName.length) {
-            rename({
-                oldName: name.oldName,
-                newName: name.oldName
+        if (!listName.newListName.length) {
+            renameList({
+                oldListName: listName.oldListName,
+                newListName: listName.oldListName
             })
             return
         }
 
-        rename({
-            oldName: name.newName,
-            newName: name.newName
+        renameList({
+            oldListName: listName.newListName,
+            newListName: listName.newListName
         })
 
         boardDispatch({
@@ -66,10 +77,43 @@ export default function List(props) {
         // send new name to server
     }
 
-    const enterNewName = e => {
+    const checkNewListName = e => {
         if (e.key !== 'Enter') return
-        nameRef.current.blur()
+        listNameRef.current.blur()
     }
+
+    const newCardkeyCheck = e => {
+        if (e.key === 'Enter') {
+            createNewCard()
+        }
+        else if (e.key === 'Escape') {
+            setNewCardName('')
+            setEdittingCardName(!edittingCardName)
+        }
+    }
+
+    const setAnchor = (e, type) => {
+        setAnchorEl({
+            listOpen: type === 'LIST',
+            newCardOpen: type === 'NEW_CARD',
+            element: e.currentTarget
+        })
+    }
+
+    const closeAnchor = type => {
+        setAnchorEl({
+            listOpen: false,
+            newCardOpen: false,
+            element: null
+        })
+    }
+
+    const createNewCard = () => {
+        // do something
+    }
+
+
+    console.log('re-render');
 
     return (
         <Draggable draggableId={props.listId} index={props.index}>
@@ -81,20 +125,76 @@ export default function List(props) {
                     <ListContent>
                         <HeaderField {...provided.dragHandleProps}>
                             <div
-                                className={`drag-target ${edittingName ? 'hide' : ''}`}
-                                onClick={() => (setEdittingName(!edittingName))}
+                                className={`drag-target ${edittingListName ? 'hide' : ''}`}
+                                onClick={() => {
+                                    setEdittingListName(!edittingListName)
+                                    setSelectFocus('list')
+                                }}
                             />
                             <TextareaAutosize
                                 rowsMax={5}
-                                ref={nameRef}
+                                ref={listNameRef}
                                 onBlur={listNameBlur}
-                                value={name.newName}
-                                onKeyPress={enterNewName}
+                                value={listName.newListName}
+                                onKeyPress={checkNewListName}
                                 onChange={handleChangeName}
                             />
-                            <Button>
-                                <div className="fas fa-ellipsis-h" />
-                            </Button>
+                            <div>
+                                <Button onClick={e => setAnchor(e, 'LIST')} style={{ fontSize: 12 }}>
+                                    <div className="fas fa-ellipsis-h" />
+                                </Button>
+                                <Popover
+                                    open={anchorEl.listOpen}
+                                    anchorEl={anchorEl.element}
+                                    onClose={() => closeAnchor('LIST')}
+                                    anchorOrigin={{
+                                        vertical: 'bottom',
+                                        horizontal: 'left',
+                                    }}
+                                    transformOrigin={{
+                                        vertical: 'top',
+                                        horizontal: 'left',
+                                    }}
+                                >
+                                    <PopOverList>
+                                        <div className='pop-over-header'>
+                                            <div className='list-act'>List Actions</div>
+                                            <div className='x-button' onClick={() => closeAnchor('LIST')}>X</div>
+                                        </div>
+
+                                        <Divider variant='middle' />
+
+                                        <div className='pop-over-content'>
+                                            <ul>
+                                                <li>Add Card</li>
+                                                <li>Copy List</li>
+                                                <li>Move List</li>
+                                                <li>Watch</li>
+                                            </ul>
+
+                                            <Divider variant='middle' />
+
+                                            <ul>
+                                                <li>Sort By</li>
+                                            </ul>
+
+                                            <Divider variant='middle' />
+
+                                            <ul>
+                                                <li>Move All Card in This List</li>
+                                                <li>Archive All Card in This List</li>
+                                            </ul>
+
+                                            <Divider variant='middle' />
+
+                                            <ul>
+                                                <li>Archive This List</li>
+                                            </ul>
+
+                                        </div>
+                                    </PopOverList>
+                                </Popover>
+                            </div>
                         </HeaderField>
 
                         <Droppable droppableId={props.listId} type='card'>
@@ -121,20 +221,77 @@ export default function List(props) {
                             )}
                         </Droppable>
                         <AddCard>
-                            {
-                                !edittingCardName ?
+                            <div
+                                className={`add-composer ${!edittingCardName ? '' : 'hide'}`}
+                                onClick={() => {
+                                    setEdittingCardName(!edittingCardName)
+                                    setSelectFocus('card')
+                                }}>
+                                <i className="fas fa-plus" ></i> Add another card
+                            </div>
+
+                            <div className={`new-card-wrapper ${edittingCardName ? '' : 'hide'}`}>
+                                <TextareaAutosize
+                                    rowsMax={7}
+                                    ref={newCardRef}
+                                    value={newCardName}
+                                    onKeyDown={newCardkeyCheck}
+                                    onChange={handleNewCardName}
+                                    placeholder='Enter a title for this card.'
+                                />
+                                <div className='add-card-wrapper'>
+                                    <Button
+                                        className='add-button'
+                                        onClick={() => {
+                                            setEdittingCardName(!edittingCardName)
+                                            createNewCard()
+                                        }}>
+                                        Add card</Button>
                                     <div
-                                        className='add-composer'
-                                        onClick={() => setEdittingCardName(!edittingCardName)}
-                                    >
-                                        <i className="fas fa-plus" ></i> Add another card
+                                        className='x-button'
+                                        onClick={() => {
+                                            setEdittingCardName(!edittingCardName)
+                                            setNewCardName('')
+                                        }}>
+                                        <i className="fas fa-times" />
                                     </div>
-                                    :
-                                    <TextareaAutosize
-                                        value={newCardName}
-                                        onChange={handleNewCardName}
-                                    />
-                            }
+                                    <div style={{ display: 'inline' }}>
+                                        <Button className='ellipsis-button' onClick={e => setAnchor(e, 'NEW_CARD')}>
+                                            <div className="fas fa-ellipsis-h" />
+                                        </Button>
+                                        <Popover
+                                            open={anchorEl.newCardOpen}
+                                            anchorEl={anchorEl.element}
+                                            onClose={() => closeAnchor('NEW_CARD')}
+                                            anchorOrigin={{
+                                                vertical: 'bottom',
+                                                horizontal: 'left',
+                                            }}
+                                            transformOrigin={{
+                                                vertical: 'top',
+                                                horizontal: 'left',
+                                            }}
+                                        >
+                                            <PopOverList>
+                                                <div className='pop-over-header'>
+                                                    <div className='list-act'>Options</div>
+                                                    <div className='x-button' onClick={() => closeAnchor('LIST')}>X</div>
+                                                </div>
+
+                                                <Divider variant='middle' />
+
+                                                <div className='pop-over-content'>
+                                                    <ul>
+                                                        <li>Member</li>
+                                                        <li>Label</li>
+                                                        <li>Position</li>
+                                                    </ul>
+                                                </div>
+                                            </PopOverList>
+                                        </Popover>
+                                    </div>
+                                </div>
+                            </div>
                         </AddCard>
                     </ListContent>
                 </ListWrapper>
@@ -142,12 +299,10 @@ export default function List(props) {
         </Draggable>
     )
 }
-
 const ListWrapper = styled.div`
     max-height: 95%;
     min-width: 250px;
     max-width: 280px;
-
     margin-right: 10px;
     border-radius: 10px;
     padding-bottom: 10px;
@@ -165,11 +320,9 @@ const ListContent = styled.div`
 
 const HeaderField = styled.div`
     min-height: 40px;
-
     padding: 10px 8px;
     font-size: 20px;
     position: relative;
-
     display: flex;
     align-items: center;
     justify-content: space-between;
@@ -182,23 +335,20 @@ const HeaderField = styled.div`
 
     textarea {
         width: 180px;
-
         resize: none;
         font-size: 18px;
         font-weight: 600;
         word-break: keep-all;
-
         border: 0;
         padding: 8px 8px;
         border-radius: 5px;
-        background-color: red;
+        background-color: transparent;
 
         :focus {
             outline-color: #2F3F8F;
             background-color: white;
         }
     }
-
     .drag-target {
         cursor: pointer;
         position: absolute;
@@ -207,12 +357,10 @@ const HeaderField = styled.div`
         right: 0;
         bottom: 0;
     }
-
     .hide {
         display: none;
     }
 `
-
 const CardList = styled.div`
     padding: 10px;
     overflow-y: auto;
@@ -220,22 +368,18 @@ const CardList = styled.div`
 `
 
 const AddCard = styled.div`
-    height: 40px;
-
+    min-height: 40px;
     margin: 10px 8px;
     display: table;
 
     .add-composer {
         width: 100%;
-        height: 100%;
-
+        height: 40px;
         padding-left: 10px;
         border-radius: 3px;
-
         font-size: 15px;
         display: table-cell;
         vertical-align: middle;
-
         color: #5e6c84;
         cursor: pointer;
 
@@ -244,12 +388,123 @@ const AddCard = styled.div`
         }
     }
 
-    textarea {
-        /* width: calc(100% - 8px); */
+    .new-card-wrapper {
         width: 100%;
+    }
+
+    .new-card-wrapper textarea {
         min-height: 20px;
+        width: calc(100% - 20px);
         
+        border: 0;
         resize: none;
+        padding: 10px;
+        font-size: 10px;
         background-color: white;
+    }
+
+    .MuiButton-root {
+        min-width: 30px;
+        text-transform: none;
+    }
+
+    .add-card-wrapper {
+        height: 40px;
+    }
+
+    .add-button {
+        min-width: 60px;
+        min-height: 40px;
+        overflow: hidden;
+        display: inline-block;
+
+        color: white;
+        background-color: #61AA4F;
+
+        :hover {
+            background-color: #65BF4A;
+        }
+    }
+    
+    .x-button {
+        min-width: 40px;
+        min-height: 40px;
+
+        color: #5e6c84;
+        font-size: 15px;
+        line-height: 40px;
+        text-align: center;
+
+        display: inline-block;
+
+        :hover {
+            color: #405070;
+            cursor: pointer;
+        }
+    }
+
+    .ellipsis-button {
+        width: 50px;
+        min-height: 30px;
+        margin-top: 10px;
+
+        float: right;
+        color: #5e6c84;
+        font-size: 15px;
+        text-align: center;
+    }
+
+    .hide {
+        display: none;
+    }
+`
+
+const PopOverList = styled.div`
+    width: 280px;
+    font-size: 14px;
+
+    padding-bottom: 10px;
+
+    .pop-over-header {
+        text-align: center;
+        position: relative;
+
+        color: #5e6c84;
+    }
+
+    .pop-over-header .list-act {
+        line-height: 40px;
+    }
+
+    .pop-over-header .x-button {
+        width: 40px;
+        height: 40px;
+        line-height: 40px;
+
+        top: 0;
+        right: 0;
+        position: absolute;
+
+        cursor: pointer;
+
+        :hover {
+            color: #1e2c44;
+        }
+    }
+
+    ul {
+        list-style-type: none;
+        margin: 0;
+        padding: 0;
+    }
+    
+    li {
+        cursor: pointer;
+        line-height: 30px;
+        padding-left: 10%;
+
+        :hover {
+            background-color: rgba(9,30,66,.08);
+        }
     }
 `
